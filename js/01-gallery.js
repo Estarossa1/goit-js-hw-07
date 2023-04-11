e4ca5403ffc75f0e8e01fadc368ca4e6df5cc846
Estarossa <sasha.zabolotnuy123@gmail.com>
2023-04-11 17:30:59 +0300

    final look

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onImgClick);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(
+  `
+  <img width="auto" height="auto" src="" style="max-width: 100%; max-height: 100%;">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const clickedElement = e.target;
+  if (clickedElement.nodeName !== "IMG") return;
+  const imgSource = clickedElement.getAttribute("data-source");
+  if (!imgSource) return;
+  instance.element().querySelector("img").src = imgSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== "Escape") return;
+  instance.close();
+}
